9fe9593f1c3a035a1e83ed9c7ab4c9905b0717bb
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-11-18 17:57:11 -0300

    feat: added Session Controller

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
 const express = require('express');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello!' });
-});
+routes.post('/sessions', SessionController.store);
 
 module.exports = routes;
