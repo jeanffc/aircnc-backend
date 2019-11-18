1e937752ce7301981a6c1a923bd0a43cd4242d05
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-11-18 17:41:18 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect(
+  'mongodb+srv://omnistack9:omnistack9@cluster0-ptgo3.mongodb.net/semana09?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
