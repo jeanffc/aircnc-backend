c4b3cbc863ed4b93c28a4b182fec9ed364001450
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-11-19 17:26:15 -0300

    feat: added Spot Controller

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
 const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
+
+routes.get('/spots', SpotController.index);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 module.exports = routes;
