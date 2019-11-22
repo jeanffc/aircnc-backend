0bcf6e4964b1b04370e22be52ad17dc27ef6ffaa
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-11-22 18:35:26 -0300

    feat: added virtual thumbnail_url in Spot Model

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -16,6 +17,7 @@ mongoose.connect(
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
