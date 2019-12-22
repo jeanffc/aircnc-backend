6464f1f2ac3619da772fe9d67fe9e03039f0fb63
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-12-22 18:21:13 -0300

    feat: added approval and rejection

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 const DashboardController = require('./controllers/DashboardController');
 const BookingController = require('./controllers/BookingController');
+const ApprovalController = require('./controllers/ApprovalController');
+const RejectionController = require('./controllers/RejectionController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -19,5 +21,8 @@ routes.delete('/spots/:id', SpotController.delete);
 routes.get('/dashboard', DashboardController.show);
 
 routes.post('/spots/:spot_id/bookings', BookingController.store);
+
+routes.post('/bookings/:booking_id/approvals', ApprovalController.store);
+routes.post('/bookings/:booking_id/rejections', RejectionController.store);
 
 module.exports = routes;
