09091a0d89231760a8ad5021f5e34581f904bb27
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-12-22 12:04:12 -0300

    testing socket.io

--- src/server.js
+++ src/server.js
@@ -2,10 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+io.on('connection', socket => {
+  console.log(socket.handshake.query);
+  console.log('User ', socket.id);
+});
 
 mongoose.connect(
   'mongodb+srv://omnistack9:omnistack9@cluster0-ptgo3.mongodb.net/semana09?retryWrites=true&w=majority',
@@ -20,4 +29,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
